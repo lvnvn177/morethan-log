748f07bb79023bedad386d345704642b9be6e09d
lvnvn177 <53424393+lvnvn177@users.noreply.github.com>
2024-10-01 01:35:15 +0900

    site.config_version1

--- site.config.js
+++ site.config.js
@@ -1,13 +1,13 @@
 const CONFIG = {
   // profile setting (required)
   profile: {
-    name: "morethanmin",
+    name: "yeongho",
     image: "/avatar.svg", // If you want to create your own notion avatar, check out https://notion-avatar.vercel.app
-    role: "frontend developer",
-    bio: "I develop everything using node.",
-    email: "morethanmin.dev@gmail.com",
-    linkedin: "morethanmin",
-    github: "morethanmin",
+    role: "iOS developer",
+    bio: "creator.",
+    email: "vheh502@gmail.com",
+    linkedin: "",
+    github: "lvnvn177",
     instagram: "",
   },
   projects: [
@@ -18,8 +18,8 @@ const CONFIG = {
   ],
   // blog setting (required)
   blog: {
-    title: "morethan-log",
-    description: "welcome to morethan-log!",
+    title: "205-log",
+    description: "welcome to 205-log!",
     scheme: "dark", // 'light' | 'dark' | 'system'
   },
 
